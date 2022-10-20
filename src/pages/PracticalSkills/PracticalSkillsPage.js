25912a5bc699ef25cf678f049e59024b513579a0
Rejnszu <lukaszrejnsz@gmail.com>
2022-10-20 14:59:55 +0200

    practical skills finish i think

--- src/pages/PracticalSkills/PracticalSkillsPage.js
+++ src/pages/PracticalSkills/PracticalSkillsPage.js
@@ -4,6 +4,7 @@ import AnimatedPages from "../../components/UI/AnimatedPages";
 import Heading from "../../components/UI/Heading";
 import SmallTextElement from "../../components/UI/SmallTextElement";
 import styles from "./PracticalSkills.module.scss";
+import { motion } from "framer-motion";
 import { practicalSkills } from "../../components/PracticalSkills/PracticalSkillsList";
 import PracticalSkillItem from "../../components/PracticalSkills/PracticalSkillItem";
 const PracticalSkills = () => {
@@ -13,6 +14,7 @@ const PracticalSkills = () => {
         <Heading text="Co mogę dla Ciebie wykonać" />
         <div className={styles["practical-skills__counter-wrapper"]}>
           <Counter
+            estimated={true}
             title="Wykonanych projektów"
             number={20}
             duration={2000}
@@ -35,7 +37,10 @@ const PracticalSkills = () => {
         </div>
         <div className={styles["practical-skills__skills-wrapper"]}>
           <div className={styles["practical-skills__inner-wrapper"]}>
-            <SmallTextElement delay={3} flexPosition="center">
+            <SmallTextElement
+              delay={window.innerWidth > 1300 ? 3 : 1}
+              flexPosition="center"
+            >
               Zaskutkowało tym że potrafie wykonać:
             </SmallTextElement>
             <ul className={styles["practical-skills__list"]}>
@@ -48,8 +53,17 @@ const PracticalSkills = () => {
               })}
             </ul>
           </div>
+          <motion.span
+            initial={{ opacity: 0, y: "-100%" }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.4, delay: 2.5 }}
+            className={styles["skills__spacer"]}
+          />
           <div className={styles["practical-skills__inner-wrapper"]}>
-            <SmallTextElement delay={3} flexPosition="center">
+            <SmallTextElement
+              delay={window.innerWidth > 1300 ? 3 : 1}
+              flexPosition="center"
+            >
               Jestem w stanie zapewnić:
             </SmallTextElement>
             <ul className={styles["practical-skills__list"]}>
